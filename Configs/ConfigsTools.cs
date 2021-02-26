7707979f982a13efd18054104f7cc80318b0675b
Elias youssef <d-s-a00@hotmail.com>
2021-02-26 22:16:57 +0100

    Added documentaion

--- Configs/ConfigsTools.cs
+++ Configs/ConfigsTools.cs
@@ -8,9 +8,16 @@ namespace Configs
 {
     public abstract class ConfigsTools
     {
+        /// <summary>
+        /// The name of the configurations file. File extension included. Change this if you want to change the file's
+        /// name.
+        /// </summary>
         [JsonIgnore]
         protected static string ConfigsFileName = "Configs.json";
 
+        /// <summary>
+        /// Saves the instance that inherents from ConfigsTools to the file as json.
+        /// </summary>
         public void Save()
         {
             JsonSerializer serializer = new JsonSerializer
@@ -27,6 +34,13 @@ namespace Configs
             }
         }
 
+        /// <summary>
+        /// Gets the current configurations from the configurations file.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>An instance of the configurations file model filled with the current configurations in  the 
+        /// configurations file. Creates a new file and returns a new instance of the class/model if no file was found.
+        /// </returns>
         public static T GetConfigs<T>() where T : ConfigsTools, new()
         {
             T result;
